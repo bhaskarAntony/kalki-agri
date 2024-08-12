add1c74e1d4fbc47e9260c3fa01b738826a4cfdf
bhaskatAntony <rooparoopa7105@gmail.com>
2024-08-12 10:39:48 +0530

    all files

--- src/pages/services/Overview.jsx
+++ src/pages/services/Overview.jsx
@@ -8,10 +8,10 @@ function Overview({title, content, image}) {
                 <div className="col-md-6">
                     <h6 className="fs-6 text-green">Overview</h6>
                     <h1 className="fs-1 text-green fw-bold">{title}</h1>
-                    <p className="fs-5 text-green">{content}</p>
+                    <p className="fs-6 text-green">{content}</p>
                     <Link to="/contact-us" className="btn-orange px-3">Contact us</Link>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-md-6 mt-3 d-flex align-items-center justify-content-center">
                     <img src={image} alt={title} className="w-75 rounded-3" />
                 </div>
             </div>
